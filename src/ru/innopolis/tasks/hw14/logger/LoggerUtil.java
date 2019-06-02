250acd44aea75dfdeaceb0be05eb8b1178ebabd1
Tuchnyak <georgii.o.sv@gmail.com>
2019-06-02 16:54:04 +0300

    before branching

--- src/ru/innopolis/tasks/hw14/logger/LoggerUtil.java
+++ src/ru/innopolis/tasks/hw14/logger/LoggerUtil.java
@@ -22,7 +22,7 @@ public class LoggerUtil {
 
     public static final Logger LOGGER = LogManager.getLogger("myLogger");
 
-    static {
+    static { //todo азнести всё по веткам!
         PropertyConfigurator.configure("./src/ru/innopolis/tasks/hw14/logger/log4j.properties");
     }
 
